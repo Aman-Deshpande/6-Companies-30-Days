e69aa11d2025c1a65b8c7bbd0467e87da8f2c0e2
Aman Deshpande <amandeshpande392@gmail.com>
2023-01-14 23:59:21 +0530

    Adobe Day 5

---
+++ Adobe/Question08.cpp
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/minimum-genetic-mutation/description/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    private:
+    bool func(string a, string b){
+        bool ans = 0;
+        for(int i = 0; i<8; i++){
+            if(a[i]!=b[i]){
+                if(ans){
+                    return 0;
+                }
+                ans=1;
+            }
+        }
+
+        return ans;
+    }
+public:
+    int minMutation(string startGene, string endGene, vector<string>& bank) {
+        queue<pair<string, int>> q;
+        q.push({startGene, 0});
+        int len = bank.size();
+
+        while(!q.empty()){
+            string s = q.front().first;
+            int t = q.front().second;
+
+            if(s==endGene){
+                return t;
+            }
+            q.pop();
+
+            int changeLen = 0;
+            for(int i = 0; i<len; i++){
+                if(func(s, bank[i])){
+                    q.push({bank[i], t+1});
+                    bank.erase(bank.begin()+i);
+                    i--;
+                    len--;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
---
+++ Adobe/Question11.cpp
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/stock-price-fluctuation/description/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class StockPrice {
+public:
+    unordered_map<int, int> map;
+    priority_queue<pair<int, int>> pq; 
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq_min;
+
+    int currentPrice = 0;
+    int currentTimeStamp = 0;
+
+    StockPrice() {}
+    
+    void update(int timestamp, int price) {
+        map[timestamp]=price;
+
+        pq.push({price, timestamp});
+        pq_min.push({price, timestamp});
+
+        if(timestamp >= currentTimeStamp){
+            currentTimeStamp = timestamp;
+            currentPrice = price;
+        }
+
+        while(map[pq.top().second] != pq.top().first){
+            pq.pop();
+        }
+
+        while(map[pq_min.top().second] != pq_min.top().first){
+            pq_min.pop();
+        }
+    }
+    
+    int current() {
+        return currentPrice;
+    }
+    
+    int maximum() {
+        return pq.top().first;
+    }
+    
+    int minimum() {
+        return pq_min.top().first;
+    }
+};
+
+/**
+ * Your StockPrice object will be instantiated and called as such:
+ * StockPrice* obj = new StockPrice();
+ * obj->update(timestamp,price);
+ * int param_2 = obj->current();
+ * int param_3 = obj->maximum();
+ * int param_4 = obj->minimum();
+ */
