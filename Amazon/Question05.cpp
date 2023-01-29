da41cb1f3bd13282051e91a1721598cece6329ac
Aman Deshpande <amandeshpande392@gmail.com>
2023-01-29 18:08:48 +0530

    Amazon Day 2

---
+++ Amazon/Question05.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/maximum-sum-of-an-hourglass/description/ 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int ans = 0;
+        
+        for(int i = 0; i<n-2; i++){
+            for(int j = 0; j<m-2; j++){
+                int res = 0;
+                for(int c = j; c<j+3; c++){
+                    res+=grid[i][c]; // first row
+                    res+=grid[i+2][c]; // third row
+                }
+                res+=grid[i+1][j+1];
+                ans=max(res, ans);
+            }
+        }
+
+        return ans;
+    }
+};
